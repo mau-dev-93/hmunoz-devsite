b7c4509a43150ab13054ce3b0df0e334b1eb2f99
Hector Mauricio Muñoz Fonseca <dev.mau.mx@gmail.com>
2025-09-23 02:47:46 -0600

    feat: Restore HumanSideSection in LandingPage for complete layout

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -20,7 +20,7 @@ const LandingPage = () => {
         <ExperienceSection />
         <SkillsSection />
         <EducationSection />
-        {/*<HumanSideSection /> */}
+        <HumanSideSection />
     </>
 };
 
