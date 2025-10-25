0820c10b2ce47b100260a275bb0195a84513e111
Hector Mauricio Muñoz Fonseca <hmmunozf@gmail.com>
2025-10-25 12:57:35 -0600

    feat: Update personal branding and role descriptions across multiple sections; enhance layout and background colors for better visual consistency

--- src/config/sections.js
+++ src/config/sections.js
@@ -2,8 +2,8 @@ export const SECTIONS = [
     { id: "hero_section", label: "Inicio" },
     { id: "aboutme_section", label: "Sobre mí" },
     { id: "experience_section", label: "Experiencia" },
-    { id: "projects_section", label: "Proyectos" },
     { id: "skills_section", label: "Habilidades" },
+    { id: "projects_section", label: "Proyectos" },
     { id: "education_section", label: "Formación" },
     { id: "humanside_section", label: "Lado Humano" },
 ];
